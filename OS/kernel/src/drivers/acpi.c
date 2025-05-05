9022e3d1772aac51df4bdde2c1b5c11f96059486
Goldenkrew3000 <hungrymike@live.com>
2025-05-06 02:19:25 +1000

    acpi: Updated ACPI driver to my modern coding standard, make ACPI reboot work again, and laid groundwork for APIC

--- OS/kernel/src/drivers/acpi.c
+++ OS/kernel/src/drivers/acpi.c
@@ -1,3 +1,7 @@
+/*
+// Hojuix ACPI Driver
+*/
+
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
@@ -17,11 +21,6 @@
 
 uintptr_t hhdt_offset = 0xFFFF800000000000;
 
-// ACPI Variables
-uint64_t* fadt_table_addr = NULL;
-uint64_t* madt_table_addr = NULL;
-uint64_t* ssdt_table_addr = NULL;
-
 // Request the ACPI RSDP Location
 __attribute__((used, section(".requests")))
 static volatile struct limine_rsdp_request rsdp_request = {
@@ -29,119 +28,12 @@ static volatile struct limine_rsdp_request rsdp_request = {
     .revision = 0
 };
 
-// RSDP / XSDP Structs
-typedef struct RSDP_t {
-    char signature[8];
-    uint8_t checksum;
-    char oem_id[6];
-    uint8_t revision;
-    uint32_t rsdt_addr;
-} __attribute__((packed));
-
-typedef struct XSDP_t {
-    char signature[8];
-    uint8_t checksum;
-    char oem_id[6];
-    uint8_t revision;
-    uint32_t rsdt_addr;
-    uint32_t length;
-    uint64_t xsdt_addr;
-    uint8_t extended_checksum;
-    uint8_t reserved[3];
-} __attribute__((packed));
-struct XSDP_t* xsdp;
-
-// Generic ACPI Struct
-typedef struct {
-    char signature[4];
-    uint32_t length;
-    uint8_t revision;
-    uint8_t checksum;
-    char oem_id[6];
-    char oem_table_id[8];
-    uint32_t oem_revision;
-    uint32_t creator_id;
-    uint32_t creator_revision;
-} __attribute__((packed)) ACPI_t;
-
-// XSDT Struct (Contains Generic ACPI header and an array of pointers to other APCI tables)
-typedef struct {
-    ACPI_t acpi_header;
-    uint64_t table_pointers[];
-} __attribute__((packed)) XSDT_t;
-
-// FADT Generic Address Structure Struct
-typedef struct {
-    uint8_t address_space;
-    uint8_t bit_width;
-    uint8_t bit_offset;
-    uint8_t access_size;
-    uint64_t address;
-} __attribute__((packed)) GenericAddressStructure_t;
-
-// FADT Struct
-typedef struct {
-    ACPI_t acpi_header;
-    uint32_t firmware_ctrl;
-    uint32_t dsdt;
-    uint8_t reserved; // Field not used anymore, but kept for compatibility
-    uint8_t preferred_power_management_profile;
-    uint16_t sci_interrupt;
-    uint32_t smi_command_port;
-    uint8_t acpi_enable;
-    uint8_t acpi_disable;
-    uint8_t s4bios_req;
-    uint8_t pstate_control;
-    uint32_t pm1a_event_block;
-    uint32_t pm1b_event_block;
-    uint32_t pm1a_control_block;
-    uint32_t pm1b_control_block;
-    uint32_t pm2_control_block;
-    uint32_t pm_timer_block;
-    uint32_t gpe0_block;
-    uint32_t gpe1_block;
-    uint8_t pm1_event_length;
-    uint8_t pm1_control_length;
-    uint8_t pm2_control_length;
-    uint8_t pm_timer_length;
-    uint8_t gpe0_length;
-    uint8_t gpe1_length;
-    uint8_t gpe1_base;
-    uint8_t cstate_control;
-    uint16_t worst_c2_latency;
-    uint16_t worst_c3_latency;
-    uint16_t flush_size;
-    uint16_t flush_stride;
-    uint8_t duty_offset;
-    uint8_t duty_width;
-    uint8_t day_alarm;
-    uint8_t month_alarm;
-    uint8_t century;
-    uint16_t boot_architecture_flags;
-    uint8_t reserved2;
-    uint32_t flags;
-    GenericAddressStructure_t reset_reg;
-    uint8_t reset_value;
-    uint8_t reserved3[3];
-    uint64_t x_firmware_control;
-    uint64_t x_dsdt;
-    GenericAddressStructure_t x_pm1a_event_block;
-    GenericAddressStructure_t x_pm1b_event_block;
-    GenericAddressStructure_t x_pm1a_control_block;
-    GenericAddressStructure_t x_pm1b_control_block;
-    GenericAddressStructure_t x_pm2_control_block;
-    GenericAddressStructure_t x_pm_timer_block;
-    GenericAddressStructure_t x_gpe0_block;
-    GenericAddressStructure_t x_gpe1_block;
-} __attribute__((packed)) FADT_t;
-FADT_t* fadt;
-
 void acpi_init() {
     printf("Initializing ACPI...\n");
 
     // Get the RSDP Pointer from Limine and load the RSDP table
-    uint64_t* rsdp_addr = rsdp_request.response->address;
-    struct RSDP_t* rsdp = (struct RSDP_t*)rsdp_addr;
+    uintptr_t rsdp_addr = (uintptr_t)rsdp_request.response->address;
+    RSDP_t* rsdp = (RSDP_t*)rsdp_addr;
 
     // TODO: Check the RSDP Checksum
 
@@ -152,91 +44,155 @@ void acpi_init() {
     } else if (rsdp->revision == 2) {
         printf("ACPI 2.0 - 6.1 detected.\n");
         // In ACPI 2.0+, the XSDP is used instead of the RSDP
-        xsdp = (struct XSDP_t*)rsdp_addr;
+        XSDP_t* xsdp = (XSDP_t*)rsdp_addr;
 
         // Jump to the XSDT handler
-        acpi_handle_xsdt();
+        acpi_handle_xsdt(xsdp->xsdt_addr + hhdt_offset);
     }
 }
 
-void acpi_handle_xsdt() {
-    // Load the XSDT pointer from the XSDP into the XSDT Struct
-    uint64_t* xsdt_addr = (uint64_t*)((uintptr_t)xsdp->xsdt_addr + hhdt_offset);
+void acpi_handle_xsdt(uintptr_t xsdt_addr) {
     XSDT_t* xsdt = (XSDT_t*)xsdt_addr;
 
     // Check the checksum of the XSDT table TODO
 
     // Load pointers for the other tables
-    uint64_t* table_addrs = (uint64_t*)xsdt->table_pointers;
+    uintptr_t table_array_addr = (uintptr_t)xsdt->table_pointers;
+    uint64_t* table_array = (uint64_t*)table_array_addr;
     int table_entries = (xsdt->acpi_header.length - sizeof(ACPI_t)) / sizeof(uint64_t);
     printf("XSDT: Entry pointers: %d\n", table_entries);
 
     // List all the tables
     /*
     for (int i = 0; i < table_entries; i++) {
-        uint64_t* addr = (uint64_t*)((uintptr_t)table_addrs[i] + hhdt_offset);
-        ACPI_t* table = (ACPI_t*)addr;
+        ACPI_t* table = (ACPI_t*)(table_array[i] + hhdt_offset);
         printf("New Table: %.4s\n", table->signature);
-        //printf("Table addr: %llx ---- OR ---- %llx\n", table_addrs[i], ((uintptr_t)table_addrs[i] + hhdt_offset));
     }
-    */
+        */
 
-    //uintptr_t addr = (uintptr_t)table_addrs[0] + hhdt_offset;
-    //uintptr_t phy = vmmgr_virt_to_phys_ext(addr);
-    //uint8_t* dat = (uint8_t*)addr;
-    //printf("%02X ", dat[i]);
+    // ACPI Variables
+    //uintptr_t fadt_table_addr = 0;
+    //uintptr_t madt_table_addr = 0;
+    //uintptr_t ssdt_table_addr = 0;
 
     // Check the tables at the pointers for desired ACPI tables
+    // NOTE: On real hardware, there are multiple tables called the same, so process them as we find them
     for (int i = 0; i < table_entries; i++) {
-        uint64_t* addr = (uint64_t*)((uintptr_t)table_addrs[i] + hhdt_offset);
-        ACPI_t* table = (ACPI_t*)addr;
+        ACPI_t* table = (ACPI_t*)(table_array[i] + hhdt_offset);
 
-        // Search for certain ACPI tables
-        // NOTE: The strings are NOT null terminated, hence the use of memcmp
+        // Search for ACPI tables
+        // NOTE: The strings are NOT null terminated
         if (memcmp(table->signature, "FACP", 4) == 0) {
             // Found the FACP / FADT Table
-            fadt_table_addr = (uint64_t*)addr; // FADT is for Power Management (https://wiki.osdev.org/FADT)
+            //printf("FADT addr: %p\n", table_array[i]);
+            //acpi_handle_fadt((uintptr_t)table_array[i] + hhdt_offset); // FADT is for Power Management (https://wiki.osdev.org/FADT)
         }
 
         if (memcmp(table->signature, "APIC", 4) == 0) {
             // Found the APIC / MADT Table
-            madt_table_addr = (uint64_t*)addr; // MADT is for Clocks / Interrupts, and maybe SMP enumeration (https://wiki.osdev.org/MADT)
+            acpi_handle_madt((uintptr_t)table_array[i] + hhdt_offset); // MADT is for Clocks / Interrupts (https://wiki.osdev.org/MADT)
+            printf("MADT Addr: %p\n", table_array[i]);
         }
 
         if (memcmp(table->signature, "SSDT", 4) == 0) {
             // Found the SSDT Table
-            ssdt_table_addr = (uint64_t*)addr; // SSDT is a suppliment to DSDT (https://wiki.osdev.org/SSDT)
+            //ssdt_table_addr = addr; // SSDT is a suppliment to DSDT (https://wiki.osdev.org/SSDT)
         }
     }
 
+    /*
     // Jump to the FADT handler
     if (fadt_table_addr != NULL) {
         printf("Found FADT table address.\n");
         acpi_handle_fadt();
     }
+        */
 }
 
-void acpi_handle_fadt() {
+void acpi_handle_fadt(uintptr_t fadt_addr) {
     // Load the FADT pointer into the FADT struct
+    FADT_t* fadt = (FADT_t*)fadt_addr;
     //uint64_t* fadt_addr = (uint64_t*)fadt_table_addr; // NOTE: Offset already applied from above
-    fadt = (FADT_t*)fadt_table_addr;
+    //fadt = (FADT_t*)fadt_table_addr;
 
-    /*
+    
     printf("FADT sig: %p\n", fadt->reset_reg.address);
     printf("Reser val: %.2x\n", fadt->reset_value);
-    uintptr_t reset_reg_final = (uintptr_t)fadt->reset_reg.address + hhdt_offset;
+    uintptr_t reset_reg_final = fadt->reset_reg.address + hhdt_offset;
     printf("final addr: %p\n", reset_reg_final);
 
     printf("ACPI rebooting in 5 seconds...");
-    timer_wait(5000);
+    //timer_wait(5000);
+    for (size_t i = 0; i < 1000000000; i++) { asm volatile("nop"); }
     out8(reset_reg_final, fadt->reset_value);
-    */
 }
 
-void acpi_shutdown() { // TODO Not working...
+void acpi_handle_madt(uintptr_t madt_addr) {
+    MADT_t* madt = (MADT_t*)madt_addr;
+    uintptr_t madt_max_addr = madt_addr + (uintptr_t)madt->acpi_header.length;
+
+
+    printf("Local APIC Address: 0x%lx\n", madt->local_apic_addr);
+    printf("Local APIC Flags: %lx\n", madt->local_apic_flags);
+    printf("Length: %d\n", madt->acpi_header.length);
+
+    // Calculate the address of the start of the entries
+    uintptr_t madt_entries_start_addr = madt_addr + sizeof(MADT_t);
+    //uint8_t* entry = (uint8_t*)madt_entries_start_addr;
+    //printf("Entry type: %x\n", entry[0]);
+    //printf("Entry length: %d\n", entry[1]);
+
+    int running = 1;
+    while (running == 1) {
+        // Make array of memory location of entry start
+        uint8_t* entry = (uint8_t*)madt_entries_start_addr;
+        
+
+        if ((uintptr_t)entry >= madt_max_addr) { running = 0; }
+
+        // Calculate the address of the next entry
+        madt_entries_start_addr += entry[1];
+
+        switch (entry[0]) {
+            case 0:
+                printf("APIC Entry Type 0:\n");
+                break;
+            case 1:
+                printf("APIC Entry Type 1:\n");
+                break;
+            case 2:
+                printf("APIC Entry Type 2:\n");
+                break;
+            case 3:
+                printf("APIC Entry Type 3:\n");
+                break;
+            case 4:
+                printf("APIC Entry Type 4:\n");
+                break;
+            case 5:
+                printf("APIC Entry Type 5:\n");
+                break;
+            case 9:
+                printf("APIC Entry Type 9:\n");
+                break;
+            default:
+                printf("Unknown APIC Entry\n");
+        };
+
+        
+    }
+    
+
+    // TODO If bit 1 is set in the flags field, mask ALL PIC interrupts (but do it anyway)
+    
+    //printf("MADT Offset: 0x%lx\n", madt_off);
+}
+
+void acpi_shutdown() {  }
+/*void acpi_shutdown() { // TODO Not working...
     // Perform ACPI shutdown (Issued from SYS_SHUTDOWN)
     printf("[ACPI] Performing ACPI Shutdown...\n");
     uintptr_t reset_reg_final = (uintptr_t)fadt->reset_reg.address + hhdt_offset;
     out8(reset_reg_final, fadt->reset_value);
     asm volatile("cli; hlt;");
-}
+}*/
--- OS/kernel/src/include/drivers/acpi.h
+++ OS/kernel/src/include/drivers/acpi.h
@@ -1,9 +1,206 @@
 #ifndef _ACPI_H
 #define _ACPI_H
 
+/*
+// RSDP / XSDP
+*/
+typedef struct {
+    char signature[8];
+    uint8_t checksum;
+    char oem_id[6];
+    uint8_t revision;
+    uint32_t rsdt_addr;
+} __attribute__((packed)) RSDP_t;
+
+typedef struct {
+    char signature[8];
+    uint8_t checksum;
+    char oem_id[6];
+    uint8_t revision;
+    uint32_t rsdt_addr;
+    uint32_t length;
+    uint64_t xsdt_addr;
+    uint8_t extended_checksum;
+    uint8_t reserved[3];
+} __attribute__((packed)) XSDP_t;
+
+/*
+// XSDT
+*/
+// Generic ACPI Struct
+typedef struct {
+    char signature[4];
+    uint32_t length;
+    uint8_t revision;
+    uint8_t checksum;
+    char oem_id[6];
+    char oem_table_id[8];
+    uint32_t oem_revision;
+    uint32_t creator_id;
+    uint32_t creator_revision;
+} __attribute__((packed)) ACPI_t;
+
+// XSDT Struct
+typedef struct {
+    ACPI_t acpi_header;
+    uint64_t table_pointers[]; // Undefined length, could be 5 tables, could be 30
+} __attribute__((packed)) XSDT_t;
+
+/*
+// FADT
+*/
+// FADT Generic Address Structure Struct
+typedef struct {
+    uint8_t address_space;
+    uint8_t bit_width;
+    uint8_t bit_offset;
+    uint8_t access_size;
+    uint64_t address;
+} __attribute__((packed)) GenericAddressStructure_t;
+
+// FADT Struct
+typedef struct {
+    ACPI_t acpi_header;
+    uint32_t firmware_ctrl;
+    uint32_t dsdt;
+    uint8_t reserved; // Field not used anymore, but kept for compatibility
+    uint8_t preferred_power_management_profile;
+    uint16_t sci_interrupt;
+    uint32_t smi_command_port;
+    uint8_t acpi_enable;
+    uint8_t acpi_disable;
+    uint8_t s4bios_req;
+    uint8_t pstate_control;
+    uint32_t pm1a_event_block;
+    uint32_t pm1b_event_block;
+    uint32_t pm1a_control_block;
+    uint32_t pm1b_control_block;
+    uint32_t pm2_control_block;
+    uint32_t pm_timer_block;
+    uint32_t gpe0_block;
+    uint32_t gpe1_block;
+    uint8_t pm1_event_length;
+    uint8_t pm1_control_length;
+    uint8_t pm2_control_length;
+    uint8_t pm_timer_length;
+    uint8_t gpe0_length;
+    uint8_t gpe1_length;
+    uint8_t gpe1_base;
+    uint8_t cstate_control;
+    uint16_t worst_c2_latency;
+    uint16_t worst_c3_latency;
+    uint16_t flush_size;
+    uint16_t flush_stride;
+    uint8_t duty_offset;
+    uint8_t duty_width;
+    uint8_t day_alarm;
+    uint8_t month_alarm;
+    uint8_t century;
+    uint16_t boot_architecture_flags;
+    uint8_t reserved2;
+    uint32_t flags;
+    GenericAddressStructure_t reset_reg;
+    uint8_t reset_value;
+    uint8_t reserved3[3];
+    uint64_t x_firmware_control;
+    uint64_t x_dsdt;
+    GenericAddressStructure_t x_pm1a_event_block;
+    GenericAddressStructure_t x_pm1b_event_block;
+    GenericAddressStructure_t x_pm1a_control_block;
+    GenericAddressStructure_t x_pm1b_control_block;
+    GenericAddressStructure_t x_pm2_control_block;
+    GenericAddressStructure_t x_pm_timer_block;
+    GenericAddressStructure_t x_gpe0_block;
+    GenericAddressStructure_t x_gpe1_block;
+} __attribute__((packed)) FADT_t;
+
+/*
+// MADT
+*/
+#define ACPI_MADT_ENTRY_TYPE_0_LENGTH 8     // Processor Local APIC
+#define ACPI_MADT_ENTRY_TYPE_1_LENGTH 12    // I/O APIC
+#define ACPI_MADT_ENTRY_TYPE_2_LENGTH 10    // I/O APIC Interrupt Source Override
+#define ACPI_MADT_ENTRY_TYPE_3_LENGTH 10    // I/O APIC Non-maskable interrupt source
+#define ACPI_MADT_ENTRY_TYPE_4_LENGTH 6     // Local APIC Non-maskable interrupts
+#define ACPI_MADT_ENTRY_TYPE_5_LENGTH 12    // Local APIC Address Override
+#define ACPI_MADT_ENTRY_TYPE_9_LENGTH 16    // Processor Local x2APIC
+
+// MADT Struct
+typedef struct {
+    ACPI_t acpi_header;
+    uint32_t local_apic_addr;
+    uint32_t local_apic_flags;
+} __attribute__((packed)) MADT_t;
+
+// MADT Entry Start
+typedef struct {
+    uint8_t type;           // Entry Type
+    uint8_t length;         // Entry Length
+} __attribute__((packed)) MADT_Entry_t;
+
+// MADT Entry Type 0 Struct
+typedef struct {
+    MADT_Entry_t entry_hdr;
+    uint8_t acpi_proc_id;   // ACPI Processor ID
+    uint8_t apic_id;        // APIC ID
+    uint32_t flags;         // Flags
+} __attribute__((packed)) MADT_Type0_t;
+
+// MADT Entry Type 1 Struct
+typedef struct {
+    MADT_Entry_t entry_hdr;
+    uint8_t io_apic_id;     // I/O APIC's ID
+    uint8_t rsvd;
+    uint32_t io_apic_addr;  // I/O APIC Address
+    uint32_t gsib;          // Global System Interrupt Base
+} __attribute__((packed)) MADT_Type1_t;
+
+// MADT Entry Type 2 Struct
+typedef struct {
+    MADT_Entry_t entry_hdr;
+    uint8_t bus_source;     // Bus Source
+    uint8_t irq_source;     // IRQ Source
+    uint32_t gsi;           // Global System Interrupt
+    uint16_t flags;         // Flags
+} __attribute__((packed)) MADT_Type2_t;
+
+// MADT Entry Type 3 Struct
+typedef struct {
+    MADT_Entry_t entry_hdr;
+    uint8_t nmi_source;     // NMI Source
+    uint8_t rsvd;
+    uint16_t flags;         // Flags
+    uint32_t gsi;           // Global System Interrupt
+} __attribute__((packed)) MADT_Type3_t;
+
+// MADT Entry Type 4 Struct
+typedef struct {
+    MADT_Entry_t entry_hdr;
+    uint8_t acpi_proc_id;   // ACPI Processor ID (0xff means all)
+    uint16_t flags;         // Flags
+    uint8_t lint;           // LINT# (0 or 1)
+} __attribute__((packed)) MADT_Type4_t;
+
+// MADT Entry Type 5 Struct
+typedef struct {
+    MADT_Entry_t entry_hdr;
+    uint16_t rsvd;
+    uint64_t apic_addr;     // 64-bit physical address of the Local APIC
+} __attribute__((packed)) MADT_Type5_t;
+
+// MADT Entry Type 9 Struct
+typedef struct {
+    MADT_Entry_t entry_hdr;
+    uint16_t rsvd;
+    uint32_t x2apic_id;     // Processor's local x2APIC ID
+    uint32_t flags;         // Flags
+    uint32_t acpi_id;       // ACPI ID
+} __attribute__((packed)) MADT_Type9_t;
+
 void acpi_init();
-void acpi_handle_xsdt();
-void acpi_handle_fadt();
+void acpi_handle_xsdt(uintptr_t xsdt_addr);
+void acpi_handle_fadt(uintptr_t fadt_addr);
+void acpi_handle_madt(uintptr_t madt_addr);
 void acpi_shutdown();
 
 #endif
